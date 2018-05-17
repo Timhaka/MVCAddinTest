ceebb31293eb0576cc54db470ce4b1f4e2c6a30a
Timhaka <32096310+Timhaka@users.noreply.github.com>
2018-05-17 10:32:17 +0200

    Testing

--- MVCAddinTestWeb/Controllers/HomeController.cs
+++ MVCAddinTestWeb/Controllers/HomeController.cs
@@ -1,39 +1,102 @@
 ﻿using Microsoft.SharePoint.Client;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace MVCAddinTestWeb.Controllers
 {
     public class HomeController : Controller
     {
         [SharePointContextFilter]
-        public ActionResult Index()
+        public ActionResult Index(string SPHostUrl)
         {
             User spUser = null;
 
             var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
 
-            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            using (ClientContext ctx = spContext.CreateUserClientContextForSPHost())
             {
-                if (clientContext != null)
+                if (ctx != null)
                 {
-                    spUser = clientContext.Web.CurrentUser;
+                    spUser = ctx.Web.CurrentUser;
 
-                    clientContext.Load(spUser, user => user.Title);
+                    ctx.Load(spUser, user => user.Title);
 
-                    clientContext.ExecuteQuery();
+                    ctx.ExecuteQuery();
 
                     ViewBag.UserName = spUser.Title;
                 }
+
+                ListCollection list = ctx.Web.Lists;
+                ctx.Load(list);
+                ctx.ExecuteQuery();
+
+
+                ViewBag.spHost = SPHostUrl;
+                ViewBag.Alllists = list;
+
             }
+
+            
+
+
 
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult ShowListItems(string listid, string SPHostUrl)
+        {
+        
+
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+
+            using (ClientContext ctx = spContext.CreateUserClientContextForSPHost())
+            {
+                List listName = ctx.Web.Lists.GetById(listid.ToGuid());
+
+                ctx.Load(listName);
+                ctx.ExecuteQuery();
+
+                ListItemCollection list = listName.GetItems(CamlQuery.CreateAllItemsQuery());
+                List<String> listitemNames = new List<String>();
+                ctx.Load(list);
+                ctx.ExecuteQuery();
+
+
+                    
+
+
+                foreach (ListItem item in list)
+                {
+                    ctx.Load(item, include => include["Title"]);
+                   // listitemNames.Add(item["Title"].ToString());
+                }
+                ctx.ExecuteQuery();
+
+                foreach (ListItem item in list)
+                {
+                    listitemNames.Add(item["Title"].ToString());
+                }
+
+                string json = JsonConvert.SerializeObject(listitemNames);
+
+                ViewBag.listItems = list;
+                ViewBag.Listname = listName.Title;
+                ViewBag.JsonTest = json;
+
+
+
+                return View(list);
+
+
+            }   
+        }
+
+            public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
